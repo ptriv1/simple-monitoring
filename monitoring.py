578a09d55cb2c5ab8a7a494221f11fcc225975be
PT <trivedp18@gmail.com>
2026-02-10 14:16:04 -0500

    to edit fetch script to be interactive by prompting for the URL

--- monitoring.py
+++ monitoring.py
@@ -1,9 +1,14 @@
 import urllib.request
+import hashlib
 
 def fetch(url):
-    response = urllib.request.urlopen(url)
-    contents = response.read()
-    return contents
+   with urllib.request.urlopen(url) as response:
+        return response.read()
+
+url = input("Enter a URL to check: ")
 
 contents = fetch(url)
-print(len(contents))
+print("Content length:", len(contents))
+
+hash_value = hashlib.sha256(contents).hexdigest()
+print("Hash:", hash_value)
